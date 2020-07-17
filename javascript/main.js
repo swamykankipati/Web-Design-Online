5b9289e0e8f883c6da9b7f21ce5bdd91218c7d43
swamykankipati <kaswami507@gmail.com>
2020-07-17 12:12:59 +0530

    added js

---
+++ javascript/main.js
@@ -0,0 +1,54 @@
+var a=10;
+console.log(a)
+var aa='abc123' 
+var ac="abc1234"
+console.log("aa values"+aa+"ac values"+ac)
+var ft=10.25
+console.log("float values"+ft)
+var z=true;
+var y=false;
+document.write("z value"+z+"y value"+y)
+_abc$=10
+document.write(_abc$)
+$abc=10
+document.write($abc)
+abc_123=10
+document.write(abc_123)
+// let
+let pi=3.14
+document.write(pi)
+// identfiers
+var hello;
+document.write(hello)
+//  mutiple varibe
+ var k='swami',n='gopi', a='anusha', m='shakerer', k='reddy', m='monoj', g='govind',s='sainath'
+ document.write(n+k+"students"+a+m+k+g+s)
+ document.write ("<br>");
+  var k='swami',
+  n='gopi', 
+  a='anusha',
+  m='shakerer',
+  k='reddy',
+  m='monoj', 
+  g='govind',
+  s='sainath'
+ document.write(n+k+"students"+a+m+k+g+s)
+ tt=4.25e+6;
+ document.write(tt)
+ tt1=4.25e-6;
+ document.write(tt1)
+
+// alert('Hello World') alertbox
+ // confirm('Hello World') 
+  // prompt('Enter Message') // ok  cancel enter 
+  hi="Hello Good Morning 'Hello Im Swami'"
+  h2='Hello Good Morning "Hello Im Swami"'
+  document.write(hi)
+  document.write(h2)
+  var h3='we\'Im Hello Im Swami';
+a=10;
+b=20;
+var c
+alert(a<b);
+alert(a>b);
+alert(c)
