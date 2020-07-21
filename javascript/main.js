8574448fd2d41a1f0f908914b66092df50a8af51
swamykankipati <kaswami507@gmail.com>
2020-07-21 09:55:19 +0530

    added js

--- javascript/main.js
+++ javascript/main.js
@@ -1,54 +1,201 @@
-var a=10;
-console.log(a)
-var aa='abc123' 
-var ac="abc1234"
-console.log("aa values"+aa+"ac values"+ac)
-var ft=10.25
-console.log("float values"+ft)
-var z=true;
-var y=false;
-document.write("z value"+z+"y value"+y)
-_abc$=10
-document.write(_abc$)
-$abc=10
-document.write($abc)
-abc_123=10
-document.write(abc_123)
-// let
-let pi=3.14
-document.write(pi)
-// identfiers
-var hello;
-document.write(hello)
-//  mutiple varibe
- var k='swami',n='gopi', a='anusha', m='shakerer', k='reddy', m='monoj', g='govind',s='sainath'
- document.write(n+k+"students"+a+m+k+g+s)
- document.write ("<br>");
-  var k='swami',
-  n='gopi', 
-  a='anusha',
-  m='shakerer',
-  k='reddy',
-  m='monoj', 
-  g='govind',
-  s='sainath'
- document.write(n+k+"students"+a+m+k+g+s)
- tt=4.25e+6;
- document.write(tt)
- tt1=4.25e-6;
- document.write(tt1)
+// var a=10;
+// console.log(a)
+// var aa='abc123' 
+// var ac="abc1234"
+// console.log("aa values"+aa+"ac values"+ac)
+// var ft=10.25
+// console.log("float values"+ft)
+// var z=true;
+// var y=false;
+// document.write("z value"+z+"y value"+y)
+// _abc$=10
+// document.write(_abc$)
+// $abc=10
+// document.write($abc)
+// abc_123=10
+// document.write(abc_123)
+// // let
+// let pi=3.14
+// document.write(pi)
+// // identfiers
+// var hello;
+// document.write(hello)
+// //  mutiple varibe
+//  var k='swami',n='gopi', a='anusha', m='shakerer', k='reddy', m='monoj', g='govind',s='sainath'
+//  document.write(n+k+"students"+a+m+k+g+s)
+//  document.write ("<br>");
+//   var k='swami',
+//   n='gopi', 
+//   a='anusha',
+//   m='shakerer',
+//   k='reddy',
+//   m='monoj', 
+//   g='govind',
+//   s='sainath'
+//  document.write(n+k+"students"+a+m+k+g+s)
+//  tt=4.25e+6;
+//  document.write(tt)
+//  tt1=4.25e-6;
+//  document.write(tt1)
 
-// alert('Hello World') alertbox
- // confirm('Hello World') 
-  // prompt('Enter Message') // ok  cancel enter 
-  hi="Hello Good Morning 'Hello Im Swami'"
-  h2='Hello Good Morning "Hello Im Swami"'
-  document.write(hi)
-  document.write(h2)
-  var h3='we\'Im Hello Im Swami';
-a=10;
-b=20;
-var c
-alert(a<b);
-alert(a>b);
-alert(c)
+// // alert('Hello World') alertbox
+//  // confirm('Hello World') 
+//   // prompt('Enter Message') // ok  cancel enter 
+//   hi="Hello Good Morning 'Hello Im Swami'"
+//   h2='Hello Good Morning "Hello Im Swami"'
+//   document.write(hi)
+//   document.write(h2)
+//   var h3='we\'Im Hello Im Swami';
+// a=10;
+// b=20;
+// var c
+// // alert(a<b);
+// // alert(a>b);
+// // alert(c)
+
+// var tn=null;
+// document.write(tn)
+// // alert("Hello Good Morning" / 2)
+// // alert(2/2)
+// // alert(15/0)
+
+// // object  dictonary 
+// var day={
+// 	eving:"5-7",
+// 	morning:"6-12",
+// 	afternoon:"2-5",
+// 	night:"8-5"
+// }
+// // console.table(obj, ['firstName', 'lastName']);
+//  // console.day(obj,['eving','morning']);
+// // console.log(day)
+//  //  array collection of diff type of  data   int float string bloolean values 
+// var ar=['a123', 123, 'True', true,false, 15.5]
+// document.write(ar)
+// document.write(ar[0],ar[1],ar[2])
+
+// function function_name(argument) {
+// 	// body...
+	
+// }
+// function swami(){
+// 	document.write('Hello')
+// }
+// function swami1(){
+// 	a=10
+// 	b=20
+// 	document.write(a+b)
+// }
+//  hello=123
+//  he='1234'
+//  document.write(typeof(hello))
+//  document.write(typeof(he))
+//  a=[123,12,56,78,'abc']
+//  ab=['a','swami','gopi', 'apssdc']
+//  document.write(ab.sort())
+//  console.log(a.length)
+//  document.write(a.length)
+// document.write(a.sort())
+// conductional staments
+// if, else elseif, switch,  
+// if(){
+// 	// conduction
+// }
+// else
+// {
+// 	// 
+// }
+
+
+
+// if (){
+
+// }
+// else if (){
+
+// }
+// else{
+
+// }
+//  var age=21;
+// if (age>18){
+// 	document.write('Ur elgible for Vote')
+
+// }
+
+// 
+// 
+ // var d=new Date();
+ // switch(d.getDay()){
+ // 	case 1:
+ // 	       document.write('Today is Monday')
+ // 	       break;
+ // 	case 2:
+ // 	      document.write('Today is TueDay')
+ // 	       break;
+ // 	case 3:
+ // 	      document.write('Today is Wen')
+ // 	       break;
+ // 	case 4:
+ // 	      document.write('Today is ThueDay')
+ // 	       break;
+ // 	case 5:
+ // 	      document.write('Today is Friday')
+ // 	       break;
+ // 	case 6:
+ // 	      document.write('Today is Sat')
+ // 	       break;
+ // 	case 7:
+ // 	      document.write('Today is Sunday')
+ // 	       break;
+
+ // }
+
+//  while 
+// 
+a=1;
+while (a<11){
+document.write(a)
+a++
+}
+
+// do{
+// 	body
+// }
+// while()
+// var k=1;
+// do{
+// 	document.write(k)
+// 	k++;
+// }
+// while(k<10):
+// var b=1;
+// do{
+// document.write(b)
+// b++;
+
+// }while(b<5)
+
+// // while()
+// var k=1;
+// do{
+// 	document.write(k)
+// 	k++;
+// }while(k<10)
+
+// for (int ; cond;in/de){
+
+// }
+for (var i =1; i<10;i++){
+	document.write(i)
+}
+// for in 
+a=[1,2,3,5,6,7]
+for (var i in a){
+	document.write(a[i])
+}
+//  for off
+aa='abcdefghijklmnopqrstvwxyz'
+for(var k of aa){
+	document.write(k)
+}
